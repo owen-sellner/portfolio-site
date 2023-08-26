fb190f7cb61511efdab3464c6935ac9d4c7a414b
owen-sellner <owen.sellner@gmail.com>
2023-08-26 16:58:03 -0400

    add PDF button

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -2,6 +2,7 @@ import { useContext } from 'react';
 import { Container, Box, Typography, IconButton, Button } from '@mui/material';
 import ThemeContext from '../theme/ThemeContext';
 import ExternalLinkIcon from '../icons/ExternalLinkIcon';
+import PDF from '../../static/Resume - Owen Sellner.pdf';
 
 
 
@@ -14,7 +15,16 @@ function Experience() {
             <Typography variant='heading'>Experience</Typography>
             <Container sx={{ width: '75%' }}>
                 <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-                    <Button style={{ color: 'inherit', background: 'transparent', textTransform: 'none', alignItems: 'center' }}>
+                    <Button 
+                        href={PDF}
+                        target='_blank'
+                        style={{ 
+                            color: 'inherit', 
+                            background: 'transparent', 
+                            textTransform: 'none', 
+                            alignItems: 'center' 
+                        }}
+                    >
                         <Typography sx={{ mr: 1 }} variant='largeText'>View PDF</Typography>
                         <ExternalLinkIcon width="30px" height="30px" stroke={theme ? '#ffffff' : '#000000'} />
                     </Button>
