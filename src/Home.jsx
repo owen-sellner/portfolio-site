11f03c77b4148eb810a29721e1192585a1741a01
owen-sellner <owen.sellner@gmail.com>
2023-08-28 16:38:49 -0400

    add footer

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,28 +3,28 @@ import NavBar from './components/NavBar';
 import Title from './sections/Title';
 import Experience from './sections/Experience';
 import About from './sections/About';
+import Footer from './sections/Footer';
 
 function Home() {
 
   return (
     <>
-      <Box>
-        <Box id="home" sx={{ height: '100vh' }}>
-          <NavBar />
-          <Title />
-        </Box>
-        <Box id="about">
-          <About />
-        </Box>
-        <Box id="experience" >
-          <Experience />
-        </Box>
-        <Box id="projects" sx={{ height: '100vh' }}>
-          <Container sx={{ pt: 20 }}>
-            <Typography variant='heading'>Projects</Typography>
-          </Container>
-        </Box>
-      </Box >
+      <Box id="home" sx={{ height: '100vh' }}>
+        <NavBar />
+        <Title />
+      </Box>
+      <Box id="about">
+        <About />
+      </Box>
+      <Box id="experience">
+        <Experience />
+      </Box>
+      <Box id="projects" sx={{ height: '100vh' }}>
+        <Container sx={{ pt: 20 }}>
+          <Typography variant='heading'>Projects</Typography>
+        </Container>
+      </Box>
+      <Footer />
     </>
   );
 };
---
+++ src/sections/Footer.jsx
@@ -0,0 +1,13 @@
+import { Box, Typography } from "@mui/material";
+
+function Footer() {
+    return (
+        <Box sx={{ height: '7vh', display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: '#45AAB8'}}>
+            <Typography sx={{ color: '#ffffff'}}>
+            
+            </Typography>
+        </Box>
+    );
+};
+
+export default Footer;
