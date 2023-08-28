2c7519ee3c8dadc623ca880bdf72613ef904eb3a
owen-sellner <owen.sellner@gmail.com>
2023-08-28 10:27:09 -0400

    add menu icon

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,30 +1,37 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import ThemeContext from '../theme/ThemeContext';
-import { Box, IconButton, Typography } from '@mui/material';
+import { Box, IconButton, Typography, useMediaQuery, useTheme } from '@mui/material';
 import { Link } from 'react-scroll';
 import compassIcon from '../icons/compass.svg';
 import sunIcon from '../icons/sun.svg';
 import moonIcon from '../icons/moon.svg';
+import MenuIcon from '../icons/MenuIcon';
 
 function NavBar() {
 
     const { theme, toggleTheme } = useContext(ThemeContext);
+
+    const isMobile = useMediaQuery(useTheme().breakpoints.down("md"));
+
+    useEffect(() => {
+        console.log(isMobile);
+    }, [isMobile])
 
     return (
         <>
             <Box sx={{ height: '10vh', display: 'flex', alignItems: 'center' }}>
                 <Box style={{ zIndex: 99999 }} sx={{ position: 'fixed', width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'space-around' }}>
                     <img src={compassIcon} style={{ width: '70px', height: '70px' }} />
-                    <Link activeStyle={{ color: '#45AAB8'}} style={{ cursor: 'pointer' }} smooth spy to="home">
+                    <Link activeStyle={{ color: '#45AAB8' }} style={{ cursor: 'pointer' }} smooth spy to="home">
                         <Typography variant="navText">Home</Typography>
                     </Link>
-                    <Link activeStyle={{ color: '#45AAB8'}} style={{ cursor: 'pointer' }} smooth spy to="about">
+                    <Link activeStyle={{ color: '#45AAB8' }} style={{ cursor: 'pointer' }} smooth spy to="about">
                         <Typography variant="navText">About</Typography>
                     </Link>
-                    <Link activeStyle={{ color: '#45AAB8'}} style={{ cursor: 'pointer' }} smooth spy to="experience">
+                    <Link activeStyle={{ color: '#45AAB8' }} style={{ cursor: 'pointer' }} smooth spy to="experience">
                         <Typography variant="navText">Experience</Typography>
                     </Link>
-                    <Link activeStyle={{ color: '#45AAB8'}} style={{ cursor: 'pointer' }} smooth spy to="projects">
+                    <Link activeStyle={{ color: '#45AAB8' }} style={{ cursor: 'pointer' }} smooth spy to="projects">
                         <Typography variant="navText">Projects</Typography>
                     </Link>
                     <IconButton onClick={toggleTheme}>
@@ -32,6 +39,10 @@ function NavBar() {
                             <img src={sunIcon} style={{ width: '40px', height: '40px' }} />
                             : <img src={moonIcon} style={{ width: '40px', height: '40px' }} />}
                     </IconButton>
+                    <IconButton onClick={toggleTheme}>
+                        <MenuIcon stroke={theme ? '#ffffff' : '#525252'} />
+                    </IconButton>
+
                 </Box>
             </Box>
         </>
---
+++ src/icons/MenuIcon.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function MenuIcon(props) {
+    return (
+        <svg width="40px" height="40px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+
+            <g id="SVGRepo_bgCarrier" stroke-width="0" />
+
+            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" />
+
+            <g id="SVGRepo_iconCarrier"> <path d="M4 6H20M4 12H20M4 18H20" {...props} stroke-width="2" stroke-linecap="round" stroke-linejoin="round" /> </g>
+
+        </svg>
+    );
+}
+
+export default MenuIcon;
