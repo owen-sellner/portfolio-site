6c6f21eae3a9fb80d6f3bc8c106914a902fbd5fa
owen-sellner <owen.sellner@gmail.com>
2023-08-28 16:22:37 -0400

    update profile pic size

--- src/components/ProfilePicture.jsx
+++ src/components/ProfilePicture.jsx
@@ -5,11 +5,11 @@ import ProfilePic from '../../static/profile-picture.png';
 function ProfilePicture() {
 
     return (
-        <Box sx={{ mb: 3, display: 'flex', justifyContent: 'center', alignItems: 'center', height: '40vh', width: '40vh', background: 'linear-gradient(#45AAB8, #4565B8)' }}>
+        <Box sx={{ mb: 3, display: 'flex', justifyContent: 'center', alignItems: 'center', height: '22em', width: '22em', background: 'linear-gradient(#45AAB8, #4565B8)' }}>
             <img
                 src={ProfilePic}
                 alt="Profile Picture"
-                style={{ width: '92%', height: '92%' }}
+                style={{ width: '20em', height: '20em' }}
             />
         </Box>
     );
