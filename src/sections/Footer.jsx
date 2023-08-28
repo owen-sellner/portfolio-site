b53c4c35809ec948177c51419ae0b1ccbaa3842d
owen-sellner <owen.sellner@gmail.com>
2023-08-28 17:06:22 -0400

    remove footer text

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -2,9 +2,8 @@ import { Box, Typography } from "@mui/material";
 
 function Footer() {
     return (
-        <Box sx={{ height: '7vh', display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: '#45AAB8'}}>
-            <Typography sx={{ color: '#ffffff'}}>
-            
+        <Box sx={{ height: '7vh', display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: '#45AAB8' }}>
+            <Typography sx={{ color: '#ffffff' }}>
             </Typography>
         </Box>
     );
