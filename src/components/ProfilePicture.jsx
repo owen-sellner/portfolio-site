a401a275199b74a9f39186c0a17b3815f59fedd0
owen-sellner <owen.sellner@gmail.com>
2023-08-29 01:12:01 -0400

    fix profile pic

--- src/components/ProfilePicture.jsx
+++ src/components/ProfilePicture.jsx
@@ -1,17 +1,35 @@
-import { Box } from '@mui/material';
+import { Box, useMediaQuery, useTheme } from "@mui/material";
+import ThemeContext from "../theme/ThemeContext";
+import { useContext } from "react";
 import ProfilePic from '../../static/profile-picture.png';
+
 
 
 function ProfilePicture() {
 
+    const { theme } = useContext(ThemeContext);
+
+    const isMobile = useMediaQuery(useTheme().breakpoints.down("sm"));
+
+    const INNER_SIZE = isMobile ? '16em' : '20em';
+
     return (
-        <Box sx={{ mb: 3, display: 'flex', justifyContent: 'center', alignItems: 'center', height: '22em', width: '22em', background: 'linear-gradient(#45AAB8, #4565B8)' }}>
+        <Box
+            sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                p: 2,
+                mb: 3,
+                background: 'linear-gradient(#45AAB8, #4565B8)'
+            }}
+        >
             <img
                 src={ProfilePic}
                 alt="Profile Picture"
-                style={{ width: '20em', height: '20em' }}
+                style={{ width: INNER_SIZE, height: INNER_SIZE }}
             />
-        </Box>
+        </Box >
     );
 };
 
