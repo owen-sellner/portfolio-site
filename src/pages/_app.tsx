46d7c2e72736cad88dc3c0be3e0a2a3ad24df298
owen-sellner <owen.sellner@gmail.com>
2025-01-07 14:08:50 -0500

    Initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,18 @@
+import '@/styles/globals.css'
+import { AnimatePresence } from 'motion/react';
+import { AppProps } from 'next/app';
+import Link from 'next/link';
+
+export default function App({ Component, pageProps, router} : AppProps) {
+    return (
+        <div>
+            <div className='nav'>
+                <Link href="/">Home</Link>
+                <Link href="/test">Test</Link>
+            </div>
+            <AnimatePresence mode='wait'>
+                <Component key={router.route} {...pageProps} />
+            </AnimatePresence>
+        </div>
+    )
+}
---
+++ src/pages/test/index.tsx
@@ -0,0 +1,13 @@
+export default function Test() {
+    return (
+        <>
+            <main>
+                <h1>Test</h1>
+                <div>
+                    <p>test1</p>
+                    <p>test2</p>
+                </div>
+            </main>
+        </>
+    )
+}
